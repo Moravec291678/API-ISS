542dad74929b8eff3a9ea10b11c7f4ce52f5e5fb
David Moravec <david.moravec2@seznam.cz>
2024-10-03 17:07:46 +0200

    API - natahneme si api ISS a zobrazime, kde se nachazi prave ted na mapach

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
-function App() {
+
+const App = () => {
+
+  const url = "http://api.open-notify.org/iss-now.json"
+  const [latitude, setLatitude] = useState("")
+  const [longitude, setLongitude] = useState("")
+  const [urlMap, setUrlMap] = useState("")
+  const [urlMapGoogle, setUrlMapGoogle] = useState("")
+
+  const getCordinates = async () => {
+    const response = await fetch(url)
+    const data = await response.json()
+
+    setLatitude(data["iss_position"]["latitude"])
+    setLongitude(data["iss_position"]["longitude"])
+
+    const iss_long = data["iss_position"]["longitude"]
+    const iss_lat = data["iss_position"]["latitude"]
+
+    setUrlMap(`https://mapy.cz/zakladni?x=${iss_long}&y=${iss_lat}&z=12`)
+    setUrlMapGoogle(`https://www.google.com/maps/@${iss_lat},${iss_long},11z/data=!5m1!1e1?entry=ttu&g_ep=EgoyMDI0MDkxMS4wIKXMDSoASAFQAw%3D%3D`)
+
+
+  }
+
+  useEffect(() => {
+    getCordinates()
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text">
+      <h1>API - ISS</h1>
+      <h2>Zeměpisná šířka a délka</h2>
+      <p>X: {latitude} --- Y: {longitude}</p>
+      <div className="links">
+        <a target="_blank" href={urlMap}>Pozice ISS - Mapy.cz</a>
+        <a target="_blank" href={urlMapGoogle}>Pozice ISS - Google Maps</a>
+      </div>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
